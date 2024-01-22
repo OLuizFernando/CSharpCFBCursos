3299d13070d0582cc83556bca39dd7778fee5744
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-22 01:24:45 -0300

    logic operators

---
+++ Aula05/aula05.cs
@@ -0,0 +1,15 @@
+using System;
+class Aula05{
+
+    static void Main(){
+
+        // & = AND = E
+        // | = OR = OU
+
+        bool r1 = 0 > 1 | 1 > 0;
+        bool r2 = 0 > 1 & 1 > 0;
+
+        Console.WriteLine(r1);
+        Console.WriteLine(r2);
+    }
+}
