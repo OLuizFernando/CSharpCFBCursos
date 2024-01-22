0670d9ef255baf6c5e4b2d356cf649dc2aa7f507
Luiz Fernando de Matos Carvalho <luizfernandodematoscarvalho@gmail.com>
2024-01-22 01:07:56 -0300

    testing variables

---
+++ Aula03/aula03.cs
@@ -0,0 +1,7 @@
+using System;
+class Aula03{
+    static void Main(){
+        int n1=10, n2=5;
+        Console.WriteLine(n1 + " x " + n2 + " = " + n1*n2);
+    }
+}
---
+++ Aula04/aula04.cs
@@ -0,0 +1,9 @@
+using System;
+class Aula04{
+
+    static int n1 = 10;
+    static void Main(){
+        int n2 = 20;
+        Console.WriteLine(n1);
+    }
+}
